215384d20aa6aa4fa506617ab909f9ecf4a2147c
yyh <942136852@qq.com>
2023-04-26 18:51:40 +0800

    数据结构类优化

--- src/main/kotlin/base/ListNode.kt
+++ src/main/kotlin/base/ListNode.kt
@@ -3,14 +3,34 @@ package base
 class ListNode(var num: Int) {
     var next: ListNode? = null
 
+    fun getString() {
+        var innode = this;
+        while (innode.next != null) {
+            println(innode.num)
+            innode = innode.next!!
+        }
+        //增加最后一个节点
+        println(innode.num)
+    }
+
+    companion object {
+        fun getSampleListNode(): List<ListNode> {
+            val listNode = ListNode(2)
+            val listNode1 = ListNode(4)
+            val listNode2 = ListNode(3)
+            listNode.next = listNode1
+            listNode.next?.next = listNode2
+
+            val listNode3 = ListNode(5)
+            val listNode4 = ListNode(6)
+            val listNode5 = ListNode(4)
+            listNode3.next = listNode4
+            listNode3.next?.next = listNode5
+
+            return listOf(listNode, listNode3)
+        }
+    }
+
 }
 
-fun ListNode.getString() {
-    var innode = this;
-    while (innode.next != null) {
-        println(innode.num)
-        innode = innode.next!!
-    }
-    //增加最后一个节点
-    println(innode.num)
-}
+
--- src/main/kotlin/base/TreeNode.kt
+++ src/main/kotlin/base/TreeNode.kt
@@ -3,10 +3,28 @@ package base
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
+
+    companion object {
+        fun getSampleTree(): TreeNode {
+            val treeNode = TreeNode(11)
+            val treeNode2 = TreeNode(22)
+            val treeNode3 = TreeNode(33)
+            val treeNode4 = TreeNode(44)
+            val treeNode5 = TreeNode(55)
+            treeNode.left = treeNode2
+            treeNode.right = treeNode3
+            treeNode2.right = treeNode4
+            treeNode3.left = treeNode5
+            return treeNode
+        }
+    }
+
+    fun getString() {
+        println(this.`val`)
+        this.left?.getString()
+        this.right?.getString()
+    }
 }
 
-fun TreeNode.getString() {
-    println(this.`val`)
-    this.left?.getString()
-    this.right?.getString()
-}
+
+
